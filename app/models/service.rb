b7425468df00d66049dcabd6680ae4d3bc6ccb77
Jenny Pletner <jennykay55@gmail.com>
2016-01-05 09:51:25 -0800

    Adding devise user model and views

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,13 @@
 class Service < ActiveRecord::Base
+  belongs_to :user
+
+  validates :service_type, :presence => true
+  validates :onsite_contact_name, :presence => true
+  validates :onsite_contact_phone, :presence => true
+  validates :company_name, :presence => true
+  validates :address, :presence => true
+  validates :city, :presence => true
+  validates :state, :presence => true
+  validates :zip, :presence => true
+
 end
