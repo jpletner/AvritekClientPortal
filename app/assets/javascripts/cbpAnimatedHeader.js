7813802f37f7ffd193353d85198fd6965e46cc4c
Jenny Pletner <jennykay55@gmail.com>
2016-01-27 12:30:20 -0800

    Resolving js no method errors, removing ref to classie

--- app/assets/javascripts/cbpAnimatedHeader.js
+++ app/assets/javascripts/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -22,17 +22,6 @@ var cbpAnimatedHeader = (function() {
 				setTimeout( scrollPage, 250 );
 			}
 		}, false );
-	}
-
-	function scrollPage() {
-		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
-		}
-		else {
-			classie.remove( header, 'navbar-shrink' );
-		}
-		didScroll = false;
 	}
 
 	function scrollY() {
